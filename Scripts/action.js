f83bce93df39454b328b6822f76beb5249018166
Ben <wbcodewrite@gmail.com>
2017-09-12 23:52:08 -0400

    new background, much more attractive site

--- Scripts/action.js
+++ Scripts/action.js
@@ -17,37 +17,37 @@ $(function () {
     btn0.href = '#';
     btn0.id = "showResume";
     btn0.className = "text-white font-18 top-nav-link";
-    btn0.innerHTML = "MGN Team";
+    btn0.innerHTML = "Team MGN - Indiana University Southeast CS Capstone";
     btn0.onclick = function () { window.location.replace("Home.html"); };
     btn0Container.appendChild(btn0);
 
-    //Resume Button
-    var btn1Container = document.createElement("DIV");
-    btn1Container.className = "col-md-2 col-sm-4 text-left margin-bottom-15";
+    ////Resume Button
+    //var btn1Container = document.createElement("DIV");
+    //btn1Container.className = "col-md-2 col-sm-4 text-left margin-bottom-15";
 
-    var btn1 = document.createElement("a");
-    btn1.href = '#';
-    btn1.id = "showResume";
-    btn1.className = "text-white font-18 top-nav-link";
-    btn1.innerHTML = "View Resumes";
-    btn1.onclick = function () { window.location.replace("Resumes.html"); };
-    btn1Container.appendChild(btn1);
+    //var btn1 = document.createElement("a");
+    //btn1.href = '#';
+    //btn1.id = "showResume";
+    //btn1.className = "text-white font-18 top-nav-link";
+    //btn1.innerHTML = "View Resumes";
+    //btn1.onclick = function () { window.location.replace("Resumes.html"); };
+    //btn1Container.appendChild(btn1);
 
-    //Developer Crew Button
-    var btn2Container = document.createElement("DIV");
-    btn2Container.className = "col-md-2 col-sm-4 text-left margin-bottom-15";
+    ////Developer Crew Button
+    //var btn2Container = document.createElement("DIV");
+    //btn2Container.className = "col-md-2 col-sm-4 text-left margin-bottom-15";
 
-    var btn2 = document.createElement("a");
-    btn2.href = "#"
-    btn2.id = 'meetTheDevelopers';
-    btn2.className = "text-white font-18 top-nav-link";
-    btn2.innerHTML = "Team Members";
-    btn2.onclick = function () { window.location.replace("DeveloperCrew.html"); };
-    btn2Container.appendChild(btn2);
+    //var btn2 = document.createElement("a");
+    //btn2.href = "#"
+    //btn2.id = 'meetTheDevelopers';
+    //btn2.className = "text-white font-18 top-nav-link";
+    //btn2.innerHTML = "Team Members";
+    //btn2.onclick = function () { window.location.replace("DeveloperCrew.html"); };
+    //btn2Container.appendChild(btn2);
 
     navRoot.appendChild(btn0Container);
-    navRoot.appendChild(btn1Container);
-    navRoot.appendChild(btn2Container);
+    //navRoot.appendChild(btn1Container);
+    //navRoot.appendChild(btn2Container);
 
     var topNav = document.getElementsByClassName("topNav");
     document.body.prepend(navRoot);

6c71dd14e8b23b11a4cb75e9a2c95859bcdf8ecc
Ben <wbcodewrite@gmail.com>
2017-10-07 22:36:01 -0400

    work with table widths

--- Scripts/action.js
+++ Scripts/action.js
@@ -22,39 +22,7 @@ $(function () {
     btn0.onclick = function () { window.location.replace("Home.html"); };
     btn0Container.appendChild(btn0);
 
-    ////Resume Button
-    //var btn1Container = document.createElement("DIV");
-    //btn1Container.className = "col-md-2 col-sm-4 text-left margin-bottom-15";
-
-    //var btn1 = document.createElement("a");
-    //btn1.href = '#';
-    //btn1.id = "showResume";
-    //btn1.className = "text-white font-18 top-nav-link";
-    //btn1.innerHTML = "View Resumes";
-    //btn1.onclick = function () { window.location.replace("Resumes.html"); };
-    //btn1Container.appendChild(btn1);
-
-    ////Developer Crew Button
-    //var btn2Container = document.createElement("DIV");
-    //btn2Container.className = "col-md-2 col-sm-4 text-left margin-bottom-15";
-
-    //var btn2 = document.createElement("a");
-    //btn2.href = "#"
-    //btn2.id = 'meetTheDevelopers';
-    //btn2.className = "text-white font-18 top-nav-link";
-    //btn2.innerHTML = "Team Members";
-    //btn2.onclick = function () { window.location.replace("DeveloperCrew.html"); };
-    //btn2Container.appendChild(btn2);
-
-    navRoot.appendChild(btn0Container);
-    //navRoot.appendChild(btn1Container);
-    //navRoot.appendChild(btn2Container);
-
-    //document.body.prepend(btn0Container);
     
-    ////THIS IS NOT CURRENTLY NEEDED BUT LEFT FOR POSSIBLE FUTURE REF
-    //On document load, add click function to 'resume-click' class elements 
-    //use this to get data-resume value, which is the directory to the clicked for resume
     $('.resume-click').click(function () {
         var resumeLocation = $(this).data('resume');
         $('#embed-resume').attr({ data: resumeLocation });
